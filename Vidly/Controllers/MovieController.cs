009ce00cff046ab18a9070f816560d6d81f19649
xsedat_ev <xsedat_ev@SDBLKN>
2018-04-05 21:24:33 +0300

    Add project files.

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.DomainModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Matrix" };
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Sedat" },
+                new Customer { Name = "Pınar" }
+            };
+
+            var model = new RandomViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+
+            return View(model);
+        }
+
+        [Route("Movie/Filter/{year:int}/{month:range(1,12)}")]
+        public ActionResult FilterByYearMonth(int year, int month)
+        {
+            return Content("year = " + year + ", month = " + month);
+        }
+    }
+}
---
+++ Vidly/Models/MovieViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.DomainModels;
+
+namespace Vidly.Models
+{
+    public class RandomViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
